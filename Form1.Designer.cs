1823c385f322d2e11cee399a2ce35a456ab1597d
JanselUI-Developer <janselfritz096@gmail.com>
2020-12-17 17:30:52 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿
+namespace Minecraft_Launcher_V2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.guna2ResizeForm1 = new Guna.UI2.WinForms.Guna2ResizeForm(this.components);
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.GrBx1 = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.password = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.usermail = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.Home = new Guna.UI2.WinForms.Guna2Button();
+            this.Test = new Guna.UI2.WinForms.Guna2Button();
+            this.Beenden = new Guna.UI2.WinForms.Guna2Button();
+            this.HomeGB = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.guna2ComboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.GrBx1.SuspendLayout();
+            this.HomeGB.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.BorderRadius = 50;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.ResizeForm = false;
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.guna2PictureBox1.BorderRadius = 29;
+            this.guna2PictureBox1.FillColor = System.Drawing.Color.CornflowerBlue;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(-2, -1);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.ShadowDecoration.Parent = this.guna2PictureBox1;
+            this.guna2PictureBox1.Size = new System.Drawing.Size(56, 451);
+            this.guna2PictureBox1.TabIndex = 0;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // GrBx1
+            // 
+            this.GrBx1.BorderColor = System.Drawing.Color.Transparent;
+            this.GrBx1.Controls.Add(this.password);
+            this.GrBx1.Controls.Add(this.label2);
+            this.GrBx1.Controls.Add(this.label1);
+            this.GrBx1.Controls.Add(this.usermail);
+            this.GrBx1.Controls.Add(this.guna2Button1);
+            this.GrBx1.CustomBorderColor = System.Drawing.Color.Transparent;
+            this.GrBx1.FillColor = System.Drawing.Color.Transparent;
+            this.GrBx1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.GrBx1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.GrBx1.Location = new System.Drawing.Point(55, 25);
+            this.GrBx1.Name = "GrBx1";
+            this.GrBx1.ShadowDecoration.Parent = this.GrBx1;
+            this.GrBx1.Size = new System.Drawing.Size(745, 425);
+            this.GrBx1.TabIndex = 1;
+            this.GrBx1.Text = " ";
+            // 
+            // password
+            // 
+            this.password.AutoRoundedCorners = true;
+            this.password.BorderRadius = 14;
+            this.password.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.password.DefaultText = "";
+            this.password.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.password.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.password.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.password.DisabledState.Parent = this.password;
+            this.password.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.password.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.password.FocusedState.Parent = this.password;
+            this.password.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.password.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.password.HoverState.Parent = this.password;
+            this.password.Location = new System.Drawing.Point(5, 102);
+            this.password.Name = "password";
+            this.password.PasswordChar = '\0';
+            this.password.PlaceholderText = "";
+            this.password.SelectedText = "";
+            this.password.ShadowDecoration.Parent = this.password;
+            this.password.Size = new System.Drawing.Size(200, 30);
+            this.password.TabIndex = 4;
+            this.password.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(5, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password (opt.)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(5, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username / E-Mail";
+            // 
+            // usermail
+            // 
+            this.usermail.AutoRoundedCorners = true;
+            this.usermail.BorderRadius = 14;
+            this.usermail.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.usermail.DefaultText = "";
+            this.usermail.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.usermail.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.usermail.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.usermail.DisabledState.Parent = this.usermail;
+            this.usermail.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.usermail.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.usermail.FocusedState.Parent = this.usermail;
+            this.usermail.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.usermail.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.usermail.HoverState.Parent = this.usermail;
+            this.usermail.Location = new System.Drawing.Point(5, 38);
+            this.usermail.Name = "usermail";
+            this.usermail.PasswordChar = '\0';
+            this.usermail.PlaceholderText = "";
+            this.usermail.SelectedText = "";
+            this.usermail.ShadowDecoration.Parent = this.usermail;
+            this.usermail.Size = new System.Drawing.Size(200, 30);
+            this.usermail.TabIndex = 1;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.AutoRoundedCorners = true;
+            this.guna2Button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.guna2Button1.BorderRadius = 14;
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.FillColor = System.Drawing.Color.MidnightBlue;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(5, 138);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(200, 30);
+            this.guna2Button1.TabIndex = 0;
+            this.guna2Button1.Text = "Login";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // Home
+            // 
+            this.Home.AutoRoundedCorners = true;
+            this.Home.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.Home.BorderRadius = 21;
+            this.Home.CheckedState.Parent = this.Home;
+            this.Home.CustomImages.Parent = this.Home;
+            this.Home.FillColor = System.Drawing.Color.CornflowerBlue;
+            this.Home.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Home.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.Home.HoverState.Parent = this.Home;
+            this.Home.Location = new System.Drawing.Point(4, 63);
+            this.Home.Name = "Home";
+            this.Home.ShadowDecoration.Parent = this.Home;
+            this.Home.Size = new System.Drawing.Size(45, 45);
+            this.Home.TabIndex = 1;
+            this.Home.Text = "Home";
+            this.Home.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // Test
+            // 
+            this.Test.BackColor = System.Drawing.Color.Indigo;
+            this.Test.CheckedState.Parent = this.Test;
+            this.Test.CustomImages.Parent = this.Test;
+            this.Test.FillColor = System.Drawing.Color.CornflowerBlue;
+            this.Test.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Test.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.Test.HoverState.Parent = this.Test;
+            this.Test.Location = new System.Drawing.Point(4, 12);
+            this.Test.Name = "Test";
+            this.Test.ShadowDecoration.Parent = this.Test;
+            this.Test.Size = new System.Drawing.Size(45, 45);
+            this.Test.TabIndex = 2;
+            this.Test.Text = "Login";
+            this.Test.Click += new System.EventHandler(this.guna2Button3_Click);
+            // 
+            // Beenden
+            // 
+            this.Beenden.AutoRoundedCorners = true;
+            this.Beenden.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.Beenden.BorderRadius = 21;
+            this.Beenden.CheckedState.Parent = this.Beenden;
+            this.Beenden.CustomImages.Parent = this.Beenden;
+            this.Beenden.FillColor = System.Drawing.Color.Red;
+            this.Beenden.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.Beenden.ForeColor = System.Drawing.Color.White;
+            this.Beenden.HoverState.Parent = this.Beenden;
+            this.Beenden.Location = new System.Drawing.Point(4, 392);
+            this.Beenden.Name = "Beenden";
+            this.Beenden.ShadowDecoration.Parent = this.Beenden;
+            this.Beenden.Size = new System.Drawing.Size(45, 45);
+            this.Beenden.TabIndex = 3;
+            this.Beenden.Text = "X";
+            this.Beenden.Click += new System.EventHandler(this.guna2Button4_Click);
+            // 
+            // HomeGB
+            // 
+            this.HomeGB.BorderColor = System.Drawing.Color.Transparent;
+            this.HomeGB.Controls.Add(this.guna2ComboBox1);
+            this.HomeGB.CustomBorderColor = System.Drawing.Color.Transparent;
+            this.HomeGB.FillColor = System.Drawing.Color.Transparent;
+            this.HomeGB.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.HomeGB.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.HomeGB.Location = new System.Drawing.Point(55, 25);
+            this.HomeGB.Name = "HomeGB";
+            this.HomeGB.ShadowDecoration.Parent = this.HomeGB;
+            this.HomeGB.Size = new System.Drawing.Size(745, 425);
+            this.HomeGB.TabIndex = 4;
+            this.HomeGB.Text = " ";
+            // 
+            // guna2ComboBox1
+            // 
+            this.guna2ComboBox1.AutoRoundedCorners = true;
+            this.guna2ComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ComboBox1.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.guna2ComboBox1.BorderRadius = 17;
+            this.guna2ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.guna2ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.guna2ComboBox1.FillColor = System.Drawing.Color.CornflowerBlue;
+            this.guna2ComboBox1.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.FocusedState.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.guna2ComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.guna2ComboBox1.HoverState.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.ItemHeight = 30;
+            this.guna2ComboBox1.ItemsAppearance.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Location = new System.Drawing.Point(10, 4);
+            this.guna2ComboBox1.Name = "guna2ComboBox1";
+            this.guna2ComboBox1.ShadowDecoration.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Size = new System.Drawing.Size(261, 36);
+            this.guna2ComboBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.HomeGB);
+            this.Controls.Add(this.Beenden);
+            this.Controls.Add(this.Test);
+            this.Controls.Add(this.Home);
+            this.Controls.Add(this.GrBx1);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.GrBx1.ResumeLayout(false);
+            this.GrBx1.PerformLayout();
+            this.HomeGB.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2Button Test;
+        private Guna.UI2.WinForms.Guna2Button Home;
+        private Guna.UI2.WinForms.Guna2GroupBox GrBx1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2ResizeForm guna2ResizeForm1;
+        private Guna.UI2.WinForms.Guna2Button Beenden;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2TextBox usermail;
+        private Guna.UI2.WinForms.Guna2TextBox password;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2GroupBox HomeGB;
+        private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CmlLib.Utils;
+using CmlLib.Launcher;
+using System.Threading;
+
+namespace Minecraft_Launcher_V2
+{
+    public partial class Form1 : Form
+    {
+        private MProfileInfo[] versions;
+        private bool allowOffline = true;
+        private MSession session;
+
+        public Form1()
+        {
+            InitializeComponent();
+            HomeGB.Hide();
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            GrBx1.Hide();
+            HomeGB.Show();
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            GrBx1.Show();
+            HomeGB.Hide();
+        }
+
+        private void guna2Button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e) //Login Button
+        {
+            guna2Button1.Enabled = false;
+            if (password.Text == "")
+            {
+                if (allowOffline)
+                {
+                    if (usermail.Text == "")
+                    {
+                        MessageBox.Show("Please enter a Username");
+                        guna2Button1.Enabled = true;
+                    }
+                    else
+                    {
+                        session = MSession.GetOfflineSession(usermail.Text);
+                        MessageBox.Show("Offline/Cracked Login succeeded: " + usermail.Text);
+                        usermail.Text = "";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Password was empty. Please try again.");
+                    guna2Button1.Enabled = true;
+                }
+                return;
+            }
+            new Thread((ThreadStart)delegate
+            {
+                MSession mSession = new MLogin().Authenticate(usermail.Text, password.Text);
+                if (mSession.Result == MLoginResult.Success)
+                {
+                    MessageBox.Show("Login Success : " + mSession.Username);
+                    session = mSession;
+                    usermail.Text = "";
+                    password.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show(mSession.Result.ToString() + "\n" + mSession.Message);
+                    Invoke((MethodInvoker)delegate
+                    {
+                        guna2Button1.Enabled = true;
+                    });
+                }
+            }).Start();
+        }
+
+    }
+}
